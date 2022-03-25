c80acb6bd23e42375276e54782f620c04cc46cf1
rafaqz <rafaelschouten@gmail.com>
2022-03-25 18:40:54 +0100

    First pass at an Extent object

--- src/Extents.jl
+++ src/Extents.jl
@@ -1,5 +1,63 @@
 module Extents
 
-# Write your package code here.
+export Extent, extent, bounds
+
+"""
+    Extent
+
+    Extent(; kw...)
+    Extent(values::NamedTuple)
+
+A wrapper for a `NamedTuple` of `Interval` that
+defines the extent of an object along its dimensions.
+"""
+struct Extent{T<:Union{Tuple,NamedTuple}}
+    bounds::T
+end
+Extent(; kw...) = Extent(values(kw))
+Extent(args...) = Extent(args)
+
+bounds(ext::Extent) = getfield(ext, :bounds)
+
+function Base.getproperty(ext::Extent, key::Symbol) 
+    haskey(bounds(ext), key) || throw(ErrorException("Extent has no field $key"))
+    getproperty(bounds(ext), key)
+end
+function Base.getindex(ext::Extent, key::Symbol)
+    haskey(bounds(ext), key) || throw(ErrorException("Extent has no field $key"))
+    getindex(bounds(ext), key)
+end
+function Base.getindex(ext::Extent, i::Int)
+    haskey(bounds(ext), i) || throw(ErrorException("Extent has no field $i"))
+    getindex(bounds(ext), i)
+end
+Base.keys(ext::Extent{<:NamedTuple}) = keys(bounds(ext))
+Base.keys(ext::Extent{<:Tuple}) = throw(ArgumentError("extent has no keys")) # What to do here?
+Base.values(ext::Extent) = values(bounds(ext))
+
+function Base.:(==)(a::Extent{<:NamedTuple{K1}}, b::Extent{<:NamedTuple{K2}}) where {K1, K2}
+    length(K1) == length(K2) || return false
+    all(map(k -> k in K1, K2)) || return false
+    return all(map((k -> a[k] == b[k]), K1))
+end
+function Base.:(==)(a::Extent{<:Tuple}, b::Extent{<:Tuple})
+    values(a) == values(b)
+end
+Base.:(==)(a::Extent, b::Extent) = false
+
+function Base.show(io::IO, ::MIME"text/plain", extent::Extent)
+    print(io, "Extent")
+    show(io, bounds(extent))
+end
+
+"""
+    extent(x)
+
+Returns an [`Extent`](@ref) object, holding an `Interval`
+for each named dimension of the object.
+"""
+function extent end
+
+extent(extent::Extent) = extent
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,31 @@ using Extents
 using Test
 
 @testset "Extents.jl" begin
-    # Write your tests here.
+    ex_nt1 = Extent(X=(1, 2), Y=(3, 4)) 
+    ex_nt2 = Extent(Y=(3, 4), X=(1, 2))
+    ex_nt3 = Extent(X=(1, 2), Y=(3, 4), Z=(5.0, 6.0)) 
+    ex_tuple1 = Extent((1, 2), (3, 4))
+
+    @testset "bounds" begin
+        @test bounds(ex_tuple1) == ((1, 2), (3, 4)) 
+        @test bounds(ex_nt1) === (X=(1, 2), Y=(3, 4))
+        @test bounds(ex_nt2) === (Y=(3, 4), X=(1, 2))
+        @test bounds(ex_nt3) === (X=(1, 2), Y=(3, 4), Z=(5.0, 6.0)) 
+    end
+
+    @testset "extent" begin
+        @test extent(ex_nt1) === ex_nt1
+    end
+
+    @testset "equality" begin
+        @test ex_nt1 == ex_nt2
+        @test ex_nt1 != ex_nt3
+    end
+
+    @testset "properties" begin
+        @test keys(ex_nt1) == (:X, :Y)
+        @test_throws ArgumentError keys(ex_tuple1)
+        @test values(ex_nt1) == ((1, 2), (3, 4))
+        @test values(ex_tuple1) == ((1, 2), (3, 4))
+    end
 end
