8cc3ff7f28fcc47be83e9c03204555e12a8191df
Martijn Visser <mgvisser@gmail.com>
2022-06-24 12:10:02 +0200

    stricter inner constructor
    This now enforces 3 things:
    
    1. Numbers are Real
    2. Per dimension the extent has two Reals
    3. The extent per dimension is sorted on construction

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,26 @@ ex1 = Extent(X=(1, 2), Y=(3, 4))
 ex2 = Extent(Y=(3, 4), X=(1, 2))
 ex3 = Extent(X=(1, 2), Y=(3, 4), Z=(5.0, 6.0))
 
+@testset "constructors" begin
+    # extent per dimension is promoted to a common type
+    x = Extent(X=(1, 2.0))
+    @test x isa Extent{(:X,), Tuple{NTuple{2, Float64}}}
+    # but different types per dimension are kept
+    x = Extent(X=(1, 2), Y=(3, 4.0))
+    @test x isa Extent{(:X, :Y), Tuple{NTuple{2, Int64}, NTuple{2, Float64}}}
+    # each dimension needs a size two Tuple
+    @test_throws ErrorException Extent()
+    @test_throws MethodError Extent(X=(2,))
+    @test_throws MethodError Extent(X=(2,3,4))
+    # with Real components
+    @test_throws MethodError Extent(X=(Complex(1), Complex(2)))
+    @test_throws MethodError Extent(X=([1], [2]))
+    # extents are sorted
+    x = Extent(X=(2.0, 1), Y=(4,3))
+    @test x.X === (1.0, 2.0)
+    @test x.Y === (3, 4)
+end
+
 @testset "getindex" begin
     @test ex3[1] == ex3[:X] == (1, 2)
     @test ex3[[:X, :Z]] == ex3[(:X, :Z)] == Extent{(:X, :Z)}(((1, 2), (5.0, 6.0)))
